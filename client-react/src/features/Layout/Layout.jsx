609ad9f6e5f459b50b56d99298cef296dcda20db
Michae1-Jackson <desotovs@gmail.com>
2022-09-08 17:52:52 +0300

    add Layout component

---
+++ client-react/src/features/Layout/Layout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header.jsx';
+import Footer from '../Footer/Footer.jsx';
+import './Layout.css';
+
+function Layout() {
+  return (
+  <>
+    <Header />
+    <main>
+      <div className='container'>
+        <Outlet />
+      </div>
+    </main>
+    <Footer />
+  </>
+  );
+}
+
+export default Layout;
