05a875db339abf89bc5783d3c2c635e9a0d4f012
Vika <zhuuvika@gmail.com>
2022-09-08 19:54:29 +0300

    create tag model

---
+++ server-express/db/models/tag.js
@@ -0,0 +1,39 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Tag extends Model {
+    static associate({ PlaceTag }) {
+      Tag.place = Tag.hasMany(PlaceTag, {
+        foreignKey: 'tagId',
+      });
+    }
+  }
+  const attributes = {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  };
+
+  Tag.init(attributes, {
+    sequelize,
+    modelName: 'Tag',
+    tableName: 'Tags',
+  });
+
+  return Tag;
+};
