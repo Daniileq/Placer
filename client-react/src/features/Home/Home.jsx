84f2e39ae539b37a336eff38e9ef3458bfb7d5b9
Oskar <oskar.stud@gmail.com>
2022-09-09 14:08:52 +0300

    add h1 and h2 font styles

--- client-react/src/features/Home/Home.jsx
+++ client-react/src/features/Home/Home.jsx
@@ -8,24 +8,25 @@ import './Home.css';
 // } from '../../store/userSlice/userSlice';
 
 function Home() {
-//   const dispatch = useDispatch();
-//   const login = useSelector(getUserLogin);
-// useEffect(() => {
-//     dispatch(loadTest());
-//   }, [dispatch]);
+  //   const dispatch = useDispatch();
+  //   const login = useSelector(getUserLogin);
+  // useEffect(() => {
+  //     dispatch(loadTest());
+  //   }, [dispatch]);
 
   return (
     <div>
       <h1>Placer</h1>
       <h2>Welcome to the New Place</h2>
-      <p>{login}</p>
-    <div className='welcome_container'>
-      <img className='welcome_pic' src='http://dogtowndogtraining.com/wp-content/uploads/2012/06/300x300-061-e1340955308953.jpg'/>
-      <div className='welcome_pic_back'></div>
-      <div className='greeting'><div>Добро пожаловать</div><div>в <span id="new_place">новое место</span></div></div>
-     <a><div className='welcome-button'>Main button</div></a>
+      {/* <p>{login}</p> */}
+      <div className='welcome_container'>
+        <img className='welcome_pic' src='http://dogtowndogtraining.com/wp-content/uploads/2012/06/300x300-061-e1340955308953.jpg' />
+        <div className='welcome_pic_back'></div>
+        <h1><div className='greeting'><div>Добро пожаловать</div><div>в <span id="new_place">новое место</span></div></div></h1>
+        <a><div className='welcome-button'>Main button</div></a>
+      </div>
+      <SearchPage />
     </div>
-    <SearchPage />
   );
 }
 
