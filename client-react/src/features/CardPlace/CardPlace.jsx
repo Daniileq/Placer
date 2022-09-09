dfc5446dfbaca24751fdff3ec255e8890b9388c9
Oskar <oskar.stud@gmail.com>
2022-09-09 11:27:57 +0300

    cardPlace component with mockup data

---
+++ client-react/src/features/CardPlace/CardPlace.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './CardPlace.css';
+
+const CardPlace = () => (
+    <div className='cardPlace'>
+        <div className='cardPlaceImage'>
+            <img src="#" alt='best place'/>
+        </div>
+        <div className='cardPlaceTags'>
+            <div className='cardPlaceTag'>tag #1</div>
+            <div className='cardPlaceTag'>tag #2</div>
+        </div>
+        <div className="cardPlaceTitle">Place#1</div>
+        <p className="cardPlaceText">
+            Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Unde fugit rem iure quas
+            provident voluptatibus vel hic tenetur assumenda,
+            ratione necessitatibus ipsum esse nemo, nisi
+            illum cum facilis impedit nesciunt!
+            {/* {
+                    cardText.length ? cardText : cardText// проверка на длину текста из БД
+                } */}
+
+        </p>
+    </div>
+);
+
+export default CardPlace;
