4ca6a7be360239a1790378e547d3b7ce9f225141
Данил Ледяев <danilledaev@MacBook-Pro-Danil.local>
2022-09-09 13:22:07 +0300

    first_version_navigation

--- client-react/src/features/Navigation/Navigation.jsx
+++ client-react/src/features/Navigation/Navigation.jsx
@@ -1,15 +1,34 @@
-import { NavLink } from 'react-router-dom';
-import './Navigation.css';
+// eslint-disable-next-line quotes
+import { NavLink } from "react-router-dom";
+// eslint-disable-next-line quotes
+import logo from "./img/logo.png";
+// eslint-disable-next-line quotes
+import "./Navigation.css";
 
 function Navigation() {
   return (
-  <nav>
-    <ul>
-      <li>
-        <NavLink to='/'>home</NavLink>
-      </li>
-    </ul>
-  </nav>
+    <div>
+      <div className="header_navigathion">
+        <div className="">
+          <NavLink className="header_nav_log_placer" to="/">
+            <img src={logo} alt="placer" />
+          </NavLink>
+        </div>
+        <div>
+          <button className="header_button_reg">
+            <NavLink className="header_registragion" to="/">
+              Регистрация
+            </NavLink>
+          </button>
+        </div>
+      </div>
+      <div className="nav_center">
+        <div>Места</div>
+        <div>Projects</div>
+        <div>Blog</div>
+        <div>Войти</div>
+      </div>
+    </div>
   );
 }
 
