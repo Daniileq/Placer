b5b29a6aaf5b8a9ff70534e88fe43b93a75f929f
Vadim <akulenko_vadim@mail.ru>
2022-09-09 12:40:06 +0300

    footer: raw version

--- client-react/src/features/Footer/Footer.jsx
+++ client-react/src/features/Footer/Footer.jsx
@@ -1,8 +1,24 @@
 import './Footer.css';
+import facebook from './img/facebook.png';
+import twitter from './img/twitter.png';
+import linkedIn from './img/linkedIn.png';
 
 function Footer() {
   return (
     <footer>
+        <div className='footer_logo'>PLACER</div>
+        <div className='footer_navigation'>
+          <div className='navigation_links'>На главную</div>
+          <div className='navigation_links'>О проекте</div>
+          <div className='navigation_links'>Контакты</div>
+        </div>
+        <div className='footer_line'></div>
+        <div className='copyright'>© JAYS SPb, 2022 till infinity</div>
+        <div className='social_media'>
+          <img src={facebook} alt="facebook" />
+          <img src={twitter} alt="twitter" />
+          <img src={linkedIn} alt="linkedIn" />
+        </div>
     </footer>
   );
 }
