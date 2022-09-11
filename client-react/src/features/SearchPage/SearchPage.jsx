70376506ad2f188bcfa8192a1c3957281c1f2070
Oskar <oskar.stud@gmail.com>
2022-09-11 11:36:02 +0300

    add searchpage in conten_container

--- client-react/src/features/SearchPage/SearchPage.jsx
+++ client-react/src/features/SearchPage/SearchPage.jsx
@@ -5,49 +5,50 @@ const tags = ['tag1', 'tag2', 'tag3', 'tag4', 'tag5'];
 
 function SearchPage() {
   return (
-    <div className='search_container'>
-      <h2 id='search_place_title'>Поиск мест</h2>
+    <div className='content_container'>
+      <div className='search_container'>
+        <h2 id='search_place_title'>Поиск мест</h2>
 
-      <div className='search_content'>
-
-        <div className='search_left_column'>
-          <div className="filters_container">
-            <div className='category'>
-              <p>категории</p>
-              <div className="options_container">
-                {categories.map((category) => (
+        <div className='search_content'>
+          <div className='search_left_column'>
+            <div className="filters_container">
+              <div className='category'>
+                <p>категории</p>
+                <div className="options_container">
+                  {categories.map((category) => (
                     <button key={category}>
                       {category}
                     </button>
-                ))}
+                  ))}
+                </div>
               </div>
-            </div>
-            <div className='tags'>
-              <p>тэги</p>
-              <div className="options_container">
-              {tags.map((tag) => (
+              <div className='tags'>
+                <p>тэги</p>
+                <div className="options_container">
+                  {tags.map((tag) => (
                     <button key={tag}>
                       {tag}
                     </button>
-              ))}
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className='search_right_column'>
-          <form className='place_search_form' method="get">
-            <label htmlFor="place_input">поиск:</label>
-            <input
-              id='place_input'
-              name='place_input'
-              className='place_input'
-              type="text"
-              placeholder='...'
-            />
-          </form>
-        </div>
+          <div className='search_right_column'>
+            <form className='place_search_form' method="get">
+              <label htmlFor="place_input">поиск:</label>
+              <input
+                id='place_input'
+                name='place_input'
+                className='place_input'
+                type="text"
+                placeholder='...'
+              />
+            </form>
+          </div>
 
+        </div>
       </div>
     </div>
   );
