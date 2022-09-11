6648347a6cd6fbb82bbbe8bcece3aafee9e6043e
kisinoverskaya <kisinoverskaya94@gmail.com>
2022-09-11 14:46:40 +0300

    add bcrypt to test-users-seed

--- server-express/db/seeders/20220910105528-test-users.js
+++ server-express/db/seeders/20220910105528-test-users.js
@@ -1,10 +1,13 @@
+const bcrypt = require('bcrypt');
+
 module.exports = {
   async up(queryInterface) {
+    const password = bcrypt.hash('qwerty12345', 10);
     await queryInterface.bulkInsert('Users', [{
       email: 'vitalya@italy.it',
       login: 'Виталя',
       displayName: 'Vetal',
-      password: 'qwerty12345',
+      password,
       photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png',
       age: 28,
       sex: 'M',
@@ -17,7 +20,7 @@ module.exports = {
       email: 'lexa@gmail.com',
       login: 'Алексей',
       displayName: 'Лёха',
-      password: 'qwerty12345',
+      password,
       photo: 'https://memepedia.ru/wp-content/uploads/2017/08/%D0%B3%D0%B4%D0%B5-%D0%BB%D0%B5%D1%85%D0%B0-2.jpg',
       age: 30,
       sex: 'M',
