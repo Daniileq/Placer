d539a4518f06ff764acb4ae9686eeee38ce78998
Oskar <oskar.stud@gmail.com>
2022-09-11 10:01:04 +0300

    change classnames for home

--- client-react/src/features/Home/Home.jsx
+++ client-react/src/features/Home/Home.jsx
@@ -4,15 +4,15 @@ import './Home.css';
 function Home() {
   return (
     <div>
-      <div className='welcome_container'>
+      <div className='welcomeContainer'>
         <img
-          className='welcome_pic'
+          className='welcomePic'
           src='http://dogtowndogtraining.com/wp-content/uploads/2012/06/300x300-061-e1340955308953.jpg'
           alt='background_welcome' />
-        <div className='welcome_pic_back'></div>
-        <div className='greeting'><div>Добро пожаловать</div><div>в <span id="new_place">новое место</span></div></div>
+        <div className='welcomePicBack'></div>
+        <div className='greeting'><div>Добро пожаловать</div><div>в <span id="newPlace">новое место</span></div></div>
         <a href='#search_place_title'>
-          <div className='welcome-button'>Поиск мест</div>
+          <div className='welcomeButton'>Поиск мест</div>
         </a>
       </div>
       <SearchPage />
