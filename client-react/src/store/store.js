7dc40bb0fce7da3c6851df2f5a0366d9e63939df
Michae1-Jackson <desotovs@gmail.com>
2022-09-11 19:05:12 +0300

    add placesSlice

--- client-react/src/store/store.js
+++ client-react/src/store/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSliceReducer from './userSlice/userSlice';
+import placesSliceReducer from './placesSlice/placesSlice';
+import searchSliceReducer from './searchSlice/searchSlice';
 
 const store = configureStore({
   reducer: {
     user: userSliceReducer,
+    places: placesSliceReducer,
+    search: searchSliceReducer,
   },
 });
 
