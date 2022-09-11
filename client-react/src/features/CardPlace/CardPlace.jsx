b76fe98bc416b26bebec826f3822da124628acc3
Michae1-Jackson <desotovs@gmail.com>
2022-09-11 19:06:31 +0300

    CardPlace component refactor

--- client-react/src/features/CardPlace/CardPlace.jsx
+++ client-react/src/features/CardPlace/CardPlace.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import './CardPlace.css';
 
-const CardPlace = () => (
-    <div className='cardPlace'>
-        <div className='cardPlaceImage'>
-            {/* <img src="#" alt='best place'/> */}
-        </div>
-        <div className='cardPlaceTags'>
-            <div className='cardPlaceTag'>tag #1</div>
-            <div className='cardPlaceTag'>tag #2</div>
-        </div>
-        <h4 className="cardPlaceTitle">Place#1</h4>
-        <p className="cardPlaceText">
-            Lorem ipsum dolor sit, amet
-            consectetur adipisicing elit. Unde fugit rem iure quas
-            provident voluptatibus vel hic tenetur assumenda,
-            ratione necessitatibus ipsum esse nemo, nisi
-            illum cum facilis impedit nesciunt!
-            {/* {
-                    cardText.length ? cardText : cardText// проверка на длину текста из БД
-                } */}
-        </p>
+const CardPlace = ({ place }) => (
+  <div className='cardPlace'>
+    <div className='cardPlaceImage'>
+      {
+        place.PlaceImages
+        && place.PlaceImages.length
+        && <img
+          src={place.PlaceImages[0].src}
+          alt={place.PlaceImages[0].title}
+        />
+      }
     </div>
+    <h4 className="cardPlaceTitle">{place.title}</h4>
+    <div className='cardPlaceTags'>
+      {place.PlaceTags && place.PlaceTags
+        .map((placeTag) => (
+          <div key={placeTag.Tag.id} className='cardPlaceTag'>
+            {placeTag.Tag.title}
+          </div>
+        ))
+      }
+    </div>
+    <p className="cardPlaceText">
+      {place.description.length && place.description}
+    </p>
+  </div>
 );
 
 export default CardPlace;
