b5d6c3676667e3a33d5b67b343f2e6ca64a88b15
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-09-11 19:06:51 +0300

    create router for changing user

--- server-express/routes/apiRouter.js
+++ server-express/routes/apiRouter.js
@@ -1,8 +1,54 @@
 const apiRouter = require('express').Router();
+const { User } = require('../db/models');
 
 apiRouter.route('/')
   .get((req, res) => {
     res.json({ success: true });
+  });
+
+apiRouter.route('/changeuser:id')
+  .put(async (req, res) => {
+    console.log(123312312313);
+    try {
+      const { id } = req.params;
+      console.log(id);
+      const {
+        displayName, email, login, city, sex, age, about, password, repeatPass,
+      } = req.body;
+
+      const updatedUser = await User.update(
+        {
+          displayName,
+          email,
+          login,
+          city,
+          sex,
+          age,
+          about,
+          password,
+          repeatPass,
+        },
+        {
+          where: { id },
+          returning: true,
+        },
+      );
+      req.session.user = {
+        id: updatedUser.id,
+        email: updatedUser.email,
+        login: updatedUser.login,
+        displayName: updatedUser.displayName,
+        photo: updatedUser.photo,
+        age: updatedUser.age,
+        sex: updatedUser.sex,
+        city: updatedUser.city,
+        about: updatedUser.about,
+        isAdmin: updatedUser.isAdmin,
+      };
+      res.json({ user: req.session.user });
+    } catch (error) {
+      res.json({ error: error.message });
+    }
   });
 
 module.exports = apiRouter;
