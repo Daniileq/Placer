4afbe7af2d288864e072fb019d7be5246eab7345
Vika <zhuuvika@gmail.com>
2022-09-12 10:03:18 +0300

    delete unused import

--- client-react/src/features/Layout/Layout.jsx
+++ client-react/src/features/Layout/Layout.jsx
@@ -2,8 +2,6 @@ import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header.jsx';
 import Footer from '../Footer/Footer.jsx';
 import './Layout.css';
-import Map from '../Map/Map.jsx';
-import BigMap from '../Map/BigMap.jsx';
 
 function Layout() {
   return (
