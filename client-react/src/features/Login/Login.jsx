6cc3e4aa277f9ae16602e0e04185fff00bda21d0
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-09-12 19:05:47 +0300

    add styles for login form

--- client-react/src/features/Login/Login.jsx
+++ client-react/src/features/Login/Login.jsx
@@ -33,32 +33,37 @@ function Login() {
   return (
     <>
       <div className="App">
-        <header className="App-header">
-          <h1>Вход</h1>
-          <br />
-          <form className="loginForm" onSubmit={loginSubmit}>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              pattern="^\S+@\S+\.\S+$"
-              title='Почта должна быть указана в формате email@mail.com'
-              required
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Пароль"
-              // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-              title="Пароль должен быть не менее 8 символов, а также содержать не менее одной цифры, одной прописной и строчной буквы"
-              required
-            />
+        <div className="login_form_conteiner">
+          <form className="login_form" onSubmit={loginSubmit}>
+            <h4>Вход</h4>
+            <label htmlFor="emailInput" >Email</label>
+              <input
+                className='font_caption'
+                type="email"
+                name="email"
+                id='emailInput'
+                placeholder="Email"
+                pattern="^\S+@\S+\.\S+$"
+                title='Почта должна быть указана в формате email@mail.com'
+                required
+              />
+            <label htmlFor="passwordInput">Пароль</label>
+              <input
+                className='font_caption'
+                type="password"
+                name="password"
+                id='passwordInput'
+                placeholder="Пароль"
+                // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                title="Пароль должен быть не менее 8 символов, а также содержать не менее одной цифры, одной прописной и строчной буквы"
+                required
+              />
             { helpMessage && <div className="helpText">{helpMessage}</div>}
-            <button type="submit">
+            <button className='login_btn' type="submit">
               Войти
             </button>
           </form>
-        </header>
+        </div>
       </div>
     </>
   );
