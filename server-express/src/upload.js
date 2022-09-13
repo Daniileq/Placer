bde8d273af28f3858a45ae66d4fdb09f911c8a0a
Michae1-Jackson <desotovs@gmail.com>
2022-09-13 23:25:49 +0300

    deploy images path corrected

--- server-express/src/upload.js
+++ server-express/src/upload.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.resolve(`${__dirname}/../public/images`));
+    // cb(null, path.resolve(`${__dirname}/../public/images`));
+    cb(null, path.resolve(`${__dirname}/../../client-react/build/images`));
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
