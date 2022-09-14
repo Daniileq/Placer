8cfcb2407ed414d66bccad1e4bcc93417c29992e
Oskar <oskar.stud@gmail.com>
2022-09-14 20:36:40 +0300

    flex to list of cardplaces

--- client-react/src/features/PlacesToGo/PlacesToGo.jsx
+++ client-react/src/features/PlacesToGo/PlacesToGo.jsx
@@ -5,7 +5,7 @@ import CardPlace from '../CardPlace/CardPlace.jsx';
 function PlacesToGo() {
   const placesToGo = useSelector((state) => state.places.placesToGo);
   return (
-    <div>
+    <div className='content_container flex_container'>
       {placesToGo.map((place) => <CardPlace place={place} key={place.id} />)}
     </div>
   );
