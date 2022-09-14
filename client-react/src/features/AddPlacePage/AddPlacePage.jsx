a9b30bb2f9ec537f046a6632c2d0d1d2a3845c16
Данил Ледяев <danilledaev@MacBook-Pro-Danil.local>
2022-09-14 15:57:28 +0300

    add_marker_on_map

--- client-react/src/features/AddPlacePage/AddPlacePage.jsx
+++ client-react/src/features/AddPlacePage/AddPlacePage.jsx
@@ -8,9 +8,10 @@ function AddPlacePage() {
   const dispatch = useDispatch();
   const filters = useSelector((state) => state.search.filters);
 
-  let myPlacemark;
   useEffect(() => {
+    // eslint-disable-next-line no-undef
     ymaps.ready(() => {
+      // eslint-disable-next-line no-undef
       const myMap = new ymaps.Map(
         'map',
         {
@@ -25,48 +26,22 @@ function AddPlacePage() {
       // Слушаем клик на карте.
       myMap.events.add('click', (e) => {
         let coords = e.get('coords');
+
         console.log(coords);
         // Если метка уже создана – просто передвигаем ее.
-        if (myPlacemark) {
-          myPlacemark.geometry.setCoordinates(coords);
-        }
-        // Если нет – создаем.
-        else {
-          myPlacemark = createPlacemark(coords);
-          myMap.geoObjects.add(myPlacemark);
-          // Слушаем событие окончания перетаскивания на метке.
-          myPlacemark.events.add('dragend', () => {
-            getAddress(myPlacemark.geometry.getCoordinates());
-          });
-        }
-        getAddress(coords);
-      });
-      // Определяем адрес по координатам (обратное геокодирование).
-      function getAddress(coords) {
-        myPlacemark.properties.set('iconCaption', 'поиск...');
-        ymaps.geocode(coords).then((res) => {
-          let firstGeoObject = res.geoObjects.get(0);
+        const myPlacemark = new ymaps.Placemark(
+          [coords[0], coords[1]],
+          {},
+          {
+            preset: 'islands#blueIcon',
+          }
+        );
 
-          myPlacemark.properties.set({
-            // Формируем строку с данными об объекте.
-            iconCaption: [
-              // Название населенного пункта или вышестоящее административно-территориальное образование.
-              firstGeoObject.getLocalities().length
-                ? firstGeoObject.getLocalities()
-                : firstGeoObject.getAdministrativeAreas(),
-              // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
-              firstGeoObject.getThoroughfare() || firstGeoObject.getPremise(),
-            ]
-              .filter(Boolean)
-              .join(', '),
-            // В качестве контента балуна задаем строку с адресом объекта.
-            balloonContent: firstGeoObject.getAddressLine(),
-          });
-        });
-      }
+        myMap.geoObjects.add(myPlacemark);
+        console.log(myPlacemark);
+      });
     });
   }, []);
-  console.log(myPlacemark);
 
   useEffect(() => {
     dispatch(loadTags());
