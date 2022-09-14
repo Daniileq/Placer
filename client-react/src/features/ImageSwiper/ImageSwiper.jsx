0822195b93ffd4ac4608f7975d4fe04b93ff0284
Michae1-Jackson <desotovs@gmail.com>
2022-09-14 15:57:50 +0300

    link borders bug fix

--- client-react/src/features/ImageSwiper/ImageSwiper.jsx
+++ client-react/src/features/ImageSwiper/ImageSwiper.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-unresolved */
-import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
 import 'swiper/css';
@@ -10,26 +9,26 @@ import './ImageSwiper.css';
 function ImageSwiper({ images, img, setImg }) {
   return (
     <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        {
-          images
-          && images.map((image) => (
-            <SwiperSlide key={image.id} className='place_image_small' onClick={() => setImg(image)}>
-              <img
-                      className={img === image ? 'small_place_image active' : 'small_place_image'}
-                      src={image.src}
-                      alt={image.title}
-                    />
-              </SwiperSlide>
-          ))
-        }
+      slidesPerView={3}
+      spaceBetween={30}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+    {
+      images
+      && images.map((image) => (
+        <SwiperSlide key={image.id} className='place_image_small' onClick={() => setImg(image)}>
+          <img
+            className={img === image ? 'small_place_image active_image' : 'small_place_image'}
+            src={image.src}
+            alt={image.title}
+          />
+          </SwiperSlide>
+      ))
+    }
       </Swiper>
   );
 }
