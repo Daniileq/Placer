aa75724f6a4297f9f260a4851ee81b35ba5f5e41
Oskar <oskar.stud@gmail.com>
2022-09-14 11:14:15 +0300

    done seed test-comments

--- server-express/db/seeders/20220914073912-test-comments.js
+++ server-express/db/seeders/20220914073912-test-comments.js
@@ -1,24 +1,40 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface) {
+    const comments = [{
+      content: 'Мне норм',
+      userId: 1,
+      placeId: 1,
+    }, {
+      content: 'Сходили в это место, очень понравилось',
+      userId: 1,
+      placeId: 2,
+    },
+    {
+      content: 'Я был там. А ты?',
+      userId: 2,
+      placeId: 1,
+    },
+    {
+      content: 'Прекрасное место, приду сюда ещё раз. Если будет такая возможность.',
+      userId: 2,
+      placeId: 2,
+    },
+    {
+      content: 'Скукота...для бабушек))ну а так,чисто и красиво конечно',
+      userId: 2,
+      placeId: 3,
+    },
+    ];
+    const commentsIn = comments.map((comment) => ({
+      ...comment,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }));
+
+    await queryInterface.bulkInsert('Comments', commentsIn);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Comments');
+  },
 };
