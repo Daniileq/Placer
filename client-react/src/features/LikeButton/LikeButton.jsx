3a0bb62446cd9b84a41e54b60d0010da2621d043
Oskar <oskar.stud@gmail.com>
2022-09-16 11:11:04 +0300

    styles change for card place

--- client-react/src/features/LikeButton/LikeButton.jsx
+++ client-react/src/features/LikeButton/LikeButton.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { toggleLike } from '../../store/placesSlice/placesSliceDeprecated';
 import './LikeButton.css';
 
-const svgHeartFill = (<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+const svgHeartFill = (<svg width="24" height="24" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
   <g clipPath="url(#clip0_855_375)">
     <path fillRule="evenodd" clipRule="evenodd" d="M8 1.31405C12.438 -3.24795 23.534 4.73505 8 15.0001C-7.534 4.73605 3.562 -3.24795 8 1.31405Z" fill="#F4D3A1" />
   </g>
@@ -15,7 +15,7 @@ const svgHeartFill = (<svg width="16" height="16" viewBox="0 0 16 16" fill="none
 </svg>
 );
 
-const svgHeart = (<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+const svgHeart = (<svg width="24" height="24" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
   <g clipPath="url(#clip0_855_373)">
     <path d="M7.99999 2.74805L7.28299 2.01105C5.59999 0.281049 2.51399 0.878049 1.39999 3.05305C0.876987 4.07605 0.758987 5.55305 1.71399 7.43805C2.63399 9.25305 4.54799 11.427 7.99999 13.795C11.452 11.427 13.365 9.25305 14.286 7.43805C15.241 5.55205 15.124 4.07605 14.6 3.05305C13.486 0.878049 10.4 0.280049 8.71699 2.01005L7.99999 2.74805ZM7.99999 15C-7.33301 4.86805 3.27899 -3.03995 7.82399 1.14305C7.88399 1.19805 7.94299 1.25505 7.99999 1.31405C8.05641 1.2551 8.11512 1.19839 8.17599 1.14405C12.72 -3.04195 23.333 4.86705 7.99999 15Z" fill="#F4D3A1" />
   </g>
@@ -51,7 +51,7 @@ function LikeButton({ place }) {
     <div>
       <div className="likes_container">
         <form name="like_form" onSubmit={handleLikeSubmit}>
-          <button type="submit" className="btn">
+          <button>
             {isLiked
               ? svgHeartFill
               : svgHeart}
